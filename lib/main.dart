c2d1daa1be5ea4af7cb72a9df22deecb52b22572
RatchapoomLN <longnight2543@gmail.com>
2021-12-15 16:14:44 +0700

    Newsv.1

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:news/auth/login.dart';
+import 'package:news/auth/register.dart';
+import 'package:news/auth/reset.dart';
+import 'package:news/home/home.dart';
+
+import 'package:news/splashscreenpage.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: SplashScreenPage.page,
+      routes: {
+        SplashScreenPage.page: (_) => SplashScreenPage(),
+        LoginPage.page: (_) => LoginPage(),
+        RegisterPage.page: (_) => RegisterPage(),
+        HomePage.page: (_) => HomePage(),
+        ResetScreen.page: (_) => ResetScreen()
+      },
+    );
+  }
+}
